21bf0c44833b9813be6d314139ed532032255eed
house <housesellout@gmail.com>
2019-07-17 11:19:11 -0400

    :- removed go_uwsgi.sh script and placing in ‘shellscripts’ public repo 	- https://github.com/greenhouse/shellscripts.git

--- go_uwsgi.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install python3.6 python3.6-dev uwsgi uwsgi-src uuid-dev libcap-dev libpcre3-dev
-cd ~
-export PYTHON=python3.6
-uwsgi --build-plugin "/usr/src/uwsgi/plugins/python python36"
-sudo mv python36_plugin.so /usr/lib/uwsgi/plugins/python36_plugin.so
-sudo chmod 644 /usr/lib/uwsgi/plugins/python36_plugin.so
